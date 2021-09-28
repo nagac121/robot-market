7f2fbfa908a021800f4757f5b341ce537ebc89b2
naga <nagac121@gmail.com>
2021-09-28 15:11:30 +0530

    display images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
-import * as React from "react";
+import { React, useCallback, useEffect, useState } from "react";
 import styles from "./App.module.css";
 
+import RoboList from "./components/RoboList";
+
 function App() {
+  let [robos, setRobos] = useState([]);
+
+  const fetchRobos = useCallback(async () => {
+    const fetchRoboPromise = await fetch("http://localhost:8000/api/robots");
+    const roboJson = await fetchRoboPromise.json();
+    setRobos(roboJson.data);
+    console.log("roboJson: ", roboJson);
+  }, []);
+
+  useEffect(() => {
+    fetchRobos();
+  }, [fetchRobos]);
+
   return (
     <div className={styles.App}>
       <header className={styles.header}>Robot Market</header>
       <div>
-        <div className="grid"></div>
+        <RoboList className="RoboList" roboData={robos}></RoboList>
         <div className="cart"></div>
       </div>
     </div>
--- frontend/src/components/RoboList.js
+++ frontend/src/components/RoboList.js
@@ -1,7 +1,64 @@
-function RoboList() {
+import React, { useRef, useState } from "react";
+
+import "./RoboList.css";
+
+function RoboList(props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const imageRef = useRef();
+
+  const imagesLoaded = (parentNode) => {
+    // returns 'true' if all imgs are loaded, otherwise returns false.
+    const imgElements = [...parentNode.querySelectorAll("img")];
+    for (let i = 0; i < imgElements.length; i += 1) {
+      const img = imgElements[i];
+      if (!img.complete) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  // useEffect(() => {
+  //   console.log(`Image Urls list: ${props.roboData}`);
+  // }, [props.roboData]);
+
+  const onImgLoad = (robo, e) => {
+    const galleryRef = imageRef.current;
+    const loaded = imagesLoaded(galleryRef);
+    setIsLoading(loaded);
+  };
+
+  const onImgError = (e) => {
+    e.target.onerror = null;
+    e.target.src = "./image-not-found.png";
+  };
+
+  const renderSpinner = () => {
+    if (!isLoading) {
+      return null;
+    }
+    return <div className="loader">Loading...</div>;
+  };
+
   return (
-    <React.Fragment>
-      <p>Robos</p>
-    </React.Fragment>
+    <div className="gallery" ref={imageRef}>
+      {renderSpinner()}
+      <div className="images">
+        {props.roboData.map((robo) => {
+          return (
+            <div key={robo.image}>
+              <img
+                src={robo.image}
+                onLoad={onImgLoad.bind(this, robo)}
+                onError={onImgError.bind(this)}
+                alt=""
+              />
+              <div>{robo.name}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
+export default RoboList;
