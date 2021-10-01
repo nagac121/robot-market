fddb88636ef2270ab01336bfd728d0119fe71b5d
naga <nagac121@gmail.com>
2021-10-01 11:01:17 +0530

    cart and gallery styles updated

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -2,17 +2,25 @@ import { useSelector } from "react-redux";
 
 import "./Cart.css";
 
+import RemoveSharpIcon from "@mui/icons-material/RemoveSharp";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
+
 function Cart() {
   const cartItems = useSelector((state) => state.robo.cartItems);
-  console.log("cart1: ",cartItems)
+  console.log("cart1: ", cartItems);
 
+  // {/* ${element.qty} in Cart; */}
   return (
     <div className="Cart-section">
-      <header>CART ITEMS</header>
+      <header>MY CART</header>
       {cartItems.map((element) => (
-        <div key={element.createdAt}>
-          <div>Name: {element.name}</div>
-          <div>Qty: {element.qty}</div>
+        <div key={element.createdAt} className="cart-item">
+          <span className="cart-item-name">{element.name}</span>
+          <span className="cart-qty-section">
+            <RemoveSharpIcon className="qty-icon"/>
+            <input className="cart-qty" disabled={true} type="text" value={element.qty} />
+            <AddSharpIcon className="qty-icon"/>
+          </span>
         </div>
       ))}
     </div>
