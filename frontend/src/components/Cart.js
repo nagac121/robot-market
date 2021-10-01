2e4357badd6a1f434fced21c3688257c1491dee7
naga <nagac121@gmail.com>
2021-10-01 17:21:43 +0530

    Displaying a message, in cart , if the cart is empty.

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -32,32 +32,36 @@ function Cart() {
   return (
     <div className="cart-section">
       <header>MY CART</header>
-      {cartItems.map((element) => (
-        <div key={element.createdAt} className="cart-item">
-          <span className="cart-item-name">{element.name}</span>
-          <span className="cart-qty-section">
-            <IconButton
-              color="primary"
-              onClick={onClickDecrCart.bind(this, element)}
-            >
-              <RemoveSharpIcon className="qty-icon" />
-            </IconButton>
-            <input
-              className="cart-qty"
-              disabled={true}
-              type="text"
-              value={element.qty}
-            />
-            <IconButton
-              color="primary"
-              onClick={onClickIncrCart.bind(this, element)}
-              disabled={element.stock === 0}
-            >
-              <AddSharpIcon className="qty-icon" />
-            </IconButton>
-          </span>
-        </div>
-      ))}
+      {cartItems.length === 0 && (
+        <div style={{ fontSize: "20px" }}>Cart is empty !</div>
+      )}
+      {cartItems.length > 0 &&
+        cartItems.map((element) => (
+          <div key={element.createdAt} className="cart-item">
+            <span className="cart-item-name">{element.name}</span>
+            <span className="cart-qty-section">
+              <IconButton
+                color="primary"
+                onClick={onClickDecrCart.bind(this, element)}
+              >
+                <RemoveSharpIcon className="qty-icon" />
+              </IconButton>
+              <input
+                className="cart-qty"
+                disabled={true}
+                type="text"
+                value={element.qty}
+              />
+              <IconButton
+                color="primary"
+                onClick={onClickIncrCart.bind(this, element)}
+                disabled={element.stock === 0}
+              >
+                <AddSharpIcon className="qty-icon" />
+              </IconButton>
+            </span>
+          </div>
+        ))}
     </div>
   );
 }
