f8f21c478278dbe1686a2a3b4a2690d1bb34f055
naga <nagac121@gmail.com>
2021-10-02 19:16:06 +0530

    total price and amount displayed

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -7,25 +7,52 @@ import RemoveSharpIcon from "@mui/icons-material/RemoveSharp";
 import "./cart.css";
 
 import { roboActions } from "../store/robo-slice";
+import { useEffect, useState } from "react";
 
 function Cart() {
   const dispatch = useDispatch();
+  const [totalAmt, setTotalAmt] = useState(0);
 
   const cartItems = useSelector((state) => state.robo.cartItems);
 
-  const onClickIncrCart = (element) => {
+  useEffect(() => {
+    let amt = cartItems.reduce((sum, i) => {
+      sum = sum + i.qty * i.price;
+
+      return sum;
+    }, 0);
+    amt = parseFloat(amt)
+      .toFixed(2)
+      .toString()
+      .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+    setTotalAmt(amt);
+  }, [cartItems]);
+
+  const onClickIncrQtyBtn = (element) => {
     dispatch(
       roboActions.updateCart({
         item: element,
         userAction: "addItemToCart",
       })
     );
+    dispatch(
+      roboActions.createMaterialMap({
+        materialType: element.material,
+        userAction: "cartPlus",
+      })
+    );
   };
-  const onClickDecrCart = (element) => {
+  const onClickDecrQtyBtn = (element) => {
     dispatch(
       roboActions.updateCart({
         item: element,
         userAction: "removeItemFromCart",
+      })
+    );
+    dispatch(
+      roboActions.createMaterialMap({
+        materialType: element.material,
+        userAction: "cartMinus",
       })
     );
   };
@@ -36,13 +63,13 @@ function Cart() {
         <div style={{ fontSize: "20px" }}>Cart is empty !</div>
       )}
       {cartItems.length > 0 &&
-        cartItems.map((element) => (
+        cartItems.map((element, index) => (
           <div key={element.createdAt} className="cart-item">
             <span className="cart-item-name">{element.name}</span>
             <span className="cart-qty-section">
               <IconButton
                 color="primary"
-                onClick={onClickDecrCart.bind(this, element)}
+                onClick={onClickDecrQtyBtn.bind(this, element)}
               >
                 <RemoveSharpIcon className="qty-icon" />
               </IconButton>
@@ -54,14 +81,17 @@ function Cart() {
               />
               <IconButton
                 color="primary"
-                onClick={onClickIncrCart.bind(this, element)}
+                onClick={onClickIncrQtyBtn.bind(this, element)}
                 disabled={element.stock === 0}
               >
                 <AddSharpIcon className="qty-icon" />
               </IconButton>
             </span>
+            <div className="cart-item-name">Material: {element.material}</div>
+            <div>Total price: {element.qty * element.price} </div>
           </div>
         ))}
+      {cartItems.length !== 0 && <div> Total amount: {totalAmt}</div>}
     </div>
   );
 }
